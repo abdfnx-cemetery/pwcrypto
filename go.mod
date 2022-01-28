b46c52c0181c6655b5d157b236854202230cbc90
abdfnx <abdfnabd@gmail.com>
2022-01-28 16:31:40 +0300

    update modules, finish from `pwc.go` functions

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/abdfnx/pwcrypto
 
 go 1.17
+
+require github.com/tscholl2/siec v0.0.0-20210707234609-9bdfc483d499
--- pwc.go
+++ pwc.go
@@ -1,9 +1,12 @@
 package pwcrypto
 
 import (
+	"fmt"
 	"errors"
 	"math/big"
 	"crypto/rand"
+	"crypto/sha256"
+	"encoding/json"
 	"crypto/elliptic"
 
 	"github.com/tscholl2/siec"
@@ -130,4 +133,125 @@ func InitCurve(pw []byte, role int, curve string) (p *PWCrypto, err error) {
 	}
 
 	return
+}
+
+// Bytes just marshalls the PAKE structure so that
+// private variables are hidden.
+func (p *PWCrypto) Bytes() (b []byte) {
+	if p == nil {
+		panic("pwc is not initialized")
+	}
+
+	b, err := json.Marshal(p.Public())
+	if err != nil {
+		panic(err)
+	}
+
+	return
+}
+
+// Update will update itself with the other parties
+// PAKE and automatically determine what stage
+// and what to generate.
+func (p *PWCrypto) Update(qBytes []byte) (err error) {
+	if p == nil {
+		err = fmt.Errorf("pwc is not initialized")
+		return
+	}
+
+	var q *PWCrypto
+	err = json.Unmarshal(qBytes, &q)
+	if err != nil {
+		return
+	}
+
+	if p.Role == q.Role {
+		err = errors.New("can't have its own role")
+		return
+	}
+
+	if p.Role == 1 {
+		// copy over public variables
+		p.Xᵤ, p.Xᵥ = q.Xᵤ, q.Xᵥ
+
+		// confirm that X is on curve
+		if !p.curve.IsOnCurve(p.Xᵤ, p.Xᵥ) {
+			err = errors.New("X values not on curve")
+			return
+		}
+
+		// STEP: B computes Y
+		p.Vpwᵤ, p.Vpwᵥ = p.curve.ScalarMult(p.Vᵤ, p.Vᵥ, p.Pw)
+		p.Upwᵤ, p.Upwᵥ = p.curve.ScalarMult(p.Uᵤ, p.Uᵥ, p.Pw)
+		p.Aα = make([]byte, 32) // randomly generated secret
+		rand.Read(p.Aα)
+		p.Aαᵤ, p.Aαᵥ = p.curve.ScalarBaseMult(p.Aα)
+		p.Yᵤ, p.Yᵥ = p.curve.Add(p.Vpwᵤ, p.Vpwᵥ, p.Aαᵤ, p.Aαᵥ) // "Y"
+		// STEP: B computes Z
+		p.Zᵤ, p.Zᵥ = p.curve.Add(p.Xᵤ, p.Xᵥ, p.Upwᵤ, new(big.Int).Neg(p.Upwᵥ))
+		p.Zᵤ, p.Zᵥ = p.curve.ScalarMult(p.Zᵤ, p.Zᵥ, p.Aα)
+		// STEP: B computes k
+		// H(pw,id_P,id_Q,X,Y,Z)
+		HB := sha256.New()
+		HB.Write(p.Pw)
+		HB.Write(p.Xᵤ.Bytes())
+		HB.Write(p.Xᵥ.Bytes())
+		HB.Write(p.Yᵤ.Bytes())
+		HB.Write(p.Yᵥ.Bytes())
+		HB.Write(p.Zᵤ.Bytes())
+		HB.Write(p.Zᵥ.Bytes())
+		// STEP: B computes k
+		p.K = HB.Sum(nil)
+	} else {
+		p.Yᵤ, p.Yᵥ = q.Yᵤ, q.Yᵥ
+
+		// confirm that Y is on curve
+		if !p.curve.IsOnCurve(p.Yᵤ, p.Yᵥ) {
+			err = errors.New("Y values not on curve")
+			return
+		}
+
+		// STEP: A computes Z
+		p.Zᵤ, p.Zᵥ = p.curve.Add(p.Yᵤ, p.Yᵥ, p.Vpwᵤ, new(big.Int).Neg(p.Vpwᵥ))
+		p.Zᵤ, p.Zᵥ = p.curve.ScalarMult(p.Zᵤ, p.Zᵥ, p.Aα)
+		// STEP: A computes k
+		// H(pw,id_P,id_Q,X,Y,Z)
+		HA := sha256.New()
+		HA.Write(p.Pw)
+		HA.Write(p.Xᵤ.Bytes())
+		HA.Write(p.Xᵥ.Bytes())
+		HA.Write(p.Yᵤ.Bytes())
+		HA.Write(p.Yᵥ.Bytes())
+		HA.Write(p.Zᵤ.Bytes())
+		HA.Write(p.Zᵥ.Bytes())
+
+		p.K = HA.Sum(nil)
+	}
+
+	return
+}
+
+// SessionKey is returned, unless it is not generated
+// in which is returns an error. This function does
+// not check if it is verifies.
+func (p *PWCrypto) SessionKey() ([]byte, error) {
+	var err error
+	if p == nil {
+		err = fmt.Errorf("pwc is not initialized")
+	}
+
+	if p.K == nil {
+		err = errors.New("session key not generated")
+	}
+
+	return p.K, err
+}
+
+// HaveSessionKey returns whether a session key has been generated
+func (p *PWCrypto) HaveSessionKey() bool {
+	if p == nil {
+		return false
+	}
+
+	return p.K != nil
 }
