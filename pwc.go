f786c444df18e425d2500b75cb04ab41347810fc
abdfnx <abdfnabd@gmail.com>
2022-01-28 16:19:09 +0300

    add `PWCrypto` struct, and `Public`, `AvailableCurves` funcs

--- pwc.go
+++ pwc.go
@@ -10,3 +10,42 @@ type EllipticCurve interface {
 	ScalarMult(Bx, By *big.Int, k []byte) (*big.Int, *big.Int)
 	IsOnCurve(x, y *big.Int) bool
 }
+
+type PWCrypto struct {
+	// Public variables
+	Role   int
+	Uᵤ, Uᵥ *big.Int
+	Vᵤ, Vᵥ *big.Int
+	Xᵤ, Xᵥ *big.Int
+	Yᵤ, Yᵥ *big.Int
+
+	// Private variables
+	curve      EllipticCurve
+	Pw         []byte
+	Vpwᵤ, Vpwᵥ *big.Int
+	Upwᵤ, Upwᵥ *big.Int
+	Aα         []byte
+	Aαᵤ, Aαᵥ   *big.Int
+	Zᵤ, Zᵥ     *big.Int
+	K          []byte
+}
+
+// Public returns the public variables of PWCrypto
+func (p *PWCrypto) Public() *PWCrypto {
+	return &PWCrypto{
+		Role: p.Role,
+		Uᵤ:   p.Uᵤ,
+		Uᵥ:   p.Uᵥ,
+		Vᵤ:   p.Vᵤ,
+		Vᵥ:   p.Vᵥ,
+		Xᵤ:   p.Xᵤ,
+		Xᵥ:   p.Xᵥ,
+		Yᵤ:   p.Yᵤ,
+		Yᵥ:   p.Yᵥ,
+	}
+}
+
+// AvailableCurves returns available curves
+func AvailableCurves() []string {
+	return []string{"p521", "p256", "p384", "siec"}
+}
